13b71c246ac4bdb41df1a5a8cded004587f3c4ca
Javier C Rodriguez <alu0100505023@ull.edu.es>
2014-11-25 12:53:43 +0000

    Añadiendo modelo de bd en datamapper

---
+++ model.rb
@@ -0,0 +1,42 @@
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :url, Text
+  property :opcional, Text
+  property :email, Text
+  property :nickname, Text
+  property :idF, Serial
+  property :idG, Serial
+  property :idT, Serial
+end
+
+class Facebook
+  include DataMapper::Resource
+  property  :id, Serial
+  property  :name, String
+  property  :token, String
+
+  belongs_to  :User
+end
+
+class Twitter
+  include DataMapper::Resource
+  property  :id, Serial
+  property  :name, Strings
+  property  :token,     String
+
+  belongs_to  :User
+end
+
+class Google
+  include DataMapper::Resource
+  property  :id, Serial
+  property  :name, String
+  property  :token,     String
+
+  belongs_to  :User
+end
+
+#En este modelo un Usuario tendria cuentas asociadas de facebook, twitter y google en una relacion 1:1
+#Si se quisiera crear una relacion N:M deberia crearse una tabla intermedia y asociar la ID de usuario a las distintas ID de cuentas
