fb2fe596c191d0770d874dee8f707a64056aaae0
Jazer Abreu <sokartema@gmail.com>
2014-12-11 13:49:53 +0000

    Mas cambios en el index

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+  var box1 = document.getElementById('div1');
+  box1.scrollTop = box1.scrollHeight;
+  var box2 = document.getElementById('div2');
+  box2.scrollTop = box2.scrollHeight;
+  
+});
