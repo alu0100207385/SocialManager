d3306ae0eaca81ac5f6fb4b94fce2c18d27b2b8b
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-12-16 13:17:35 +0000

    2a fase de tests v1

--- test/test.rb
+++ test/test.rb
@@ -13,7 +13,7 @@ def app
 end
 
 
-describe "Test Chat App: Comprobacion de paginas y enlaces" do
+describe "Test Chat App: Check pages and links" do
    
    before :all do
 	  @browser = Selenium::WebDriver.for :firefox
@@ -71,4 +71,59 @@ describe "Test Chat App: Comprobacion de paginas y enlaces" do
 	  @browser.manage.timeouts.implicit_wait = 3
 	  assert_equal(@site+"help", @browser.current_url)
    end
+   
+   it "##9. I can access Recovery page" do
+	  @browser.find_element(:id,"recovery").click
+	  @browser.manage.timeouts.implicit_wait = 3
+	  assert_equal(@site+"recuperar", @browser.current_url)
+   end
+   
+#    it "##10. I can access Registration page" do
+#    it "##11. I can access Registration page and come back" do
+   
 end
+
+describe "Test Chat App: Sign in page: Log&Reg" do
+   
+   before :all do
+	  @browser = Selenium::WebDriver.for :firefox
+	  @site = 'http://localhost:9292/'
+	  @browser.get(@site)
+	  @browser.manage().window().maximize()
+	  @browser.manage.timeouts.implicit_wait = 5
+   end
+
+   after :all do
+	  @browser.quit
+   end
+   
+   it "##1. User not exist" do
+	  @browser.find_element(:id,"nickname").send_keys("someone")
+	  @browser.find_element(:id,"password").send_keys("1234")
+	  @browser.find_element(:id,"login").click
+	  @browser.manage.timeouts.implicit_wait = 5
+	  element = @browser.find_element(:id,"text").text
+	  assert_equal("The user does not exist in the database.",element)
+   end
+
+   it "##2. Registation Fail" do
+	  @browser.find_element(:id,"Registrarse").click
+	  @browser.manage.timeouts.implicit_wait = 5
+	  element = @browser.find_element(:id,"text").text
+	  assert_equal("Error, missing a field filled.",element)
+   end
+=begin
+   it "##3. Registation Ok" do
+   end
+   
+   it "##4. Log out" do
+   end
+   
+   it "##5. Log in Ok" do
+   end
+=end
+end
+
+# describe "Test Chat App: User's actions" do
+#    Postear, entrar a opciones, leer
+# end
