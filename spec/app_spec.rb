117b5af69eba3f900a145bdd1e8076065f9a56c4
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-12-18 19:37:28 +0000

    +coveralls

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -11,6 +11,7 @@ require 'rubygems'
 require 'rspec'
 require 'test/unit'
 # require 'rspec/expectations'
+require 'selenium-webdriver'
 
 include Rack::Test::Methods
 include Test::Unit::Assertions
@@ -98,5 +99,41 @@ describe "Test App: Get Methods" do
 	  link = createlink
 	  assert_equal(String,link.class)
    end
-
+   
+   it "Check login fail" do
+	  post '/login'
+	  expect(last_response.body).to eq("{\"key1\":\"error1\"}")
+   end
+   
+#    it "Check linkedin access" do
+# 	  get '/linkedin'
+# 	  expect(last_response.body).to eq("Not an ajax request")
+# 	  expect(last_response).to be_ok
+#    end
 end
+
+=begin
+describe "Test Chat App: Check pages and links" do
+   
+   before :all do
+	  @browser = Selenium::WebDriver.for :firefox
+	  @site = 'http://localhost:9292/'
+	  @browser.get(@site)
+	  @browser.manage().window().maximize()
+	  @browser.manage.timeouts.implicit_wait = 5
+   end
+
+   after :all do
+	  @browser.quit
+   end
+
+   it "##1. I can access index page" do
+	  expect(@site).to eq(@browser.current_url)
+   end
+
+   it "##2. I can see index page" do
+	  element = @browser.find_element(:tag_name,"h1").text
+	  expect("Welcome to Social Manager").to eq(element)
+   end
+end
+=end
--- test/test.rb
+++ test/test.rb
@@ -29,7 +29,7 @@ describe "Test Chat App: Check pages and links" do
    it "##1. I can access index page" do
 	  assert_equal(@site, @browser.current_url)
    end
-
+=begin
    it "##2. I can see index page" do
 	  element = @browser.find_element(:tag_name,"h1").text
 	  assert_equal("Welcome to Social Manager", element)
@@ -196,3 +196,6 @@ describe "Test Chat App: Sign in page: Log&Reg" do
 	  assert_equal(@site,@browser.current_url)
    end
  end
+=end
+   
+end
